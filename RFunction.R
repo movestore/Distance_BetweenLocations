113da0915ea6f5f94a8abeecc7900505e20a031f
annescharf <ascharf@ab.mpg.de>
2023-08-30 22:45:14 +0200

    fix path for artifact

--- RFunction.R
+++ RFunction.R
@@ -8,15 +8,15 @@ rFunction <-  function(data, units) {
 
     if(mt_n_tracks(data)==1){
       distanceHist <- ggplot(data[!is.na(data$distance),], aes(distance))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(data)]])+ xlab("Distance")+theme_bw()
-      pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "distance_histogram.pdf"))
+      pdf(appArtifactPath("distance_histogram.pdf"))
       print(distanceHist)
       dev.off()
   
       outDF <- data.frame(timestamps=mt_time(data),location.long=sf::st_coordinates(data)[,1],location.lat=sf::st_coordinates(data)[,2], track_id=mt_track_id(data), distance=data$distance)
       colnames(outDF)[5] <- paste0(colnames(outDF)[5],"_", units(data$distance)$numerator)
-      write.csv(outDF, row.names=F, file = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"distanceBetweenLocations.csv"))
+      write.csv(outDF, row.names=F, file = appArtifactPath("distanceBetweenLocations.csv"))
     } else {
-    pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "distance_histogram.pdf"))
+    pdf(appArtifactPath("distance_histogram.pdf"))
     distanceHistAll <- ggplot(data[!is.na(data$distance),], aes(distance))+geom_histogram(bins=100)+ xlab("Distance")+ggtitle("All Individuals")+ theme_bw()
     print(distanceHistAll)
     lapply(split(data, mt_track_id(data)), function(dataI){
@@ -27,7 +27,7 @@ rFunction <-  function(data, units) {
     
     outDF <- data.frame(timestamps=mt_time(data),location.long=sf::st_coordinates(data)[,1],location.lat=sf::st_coordinates(data)[,2], track_id=mt_track_id(data), distance=data$distance)
     colnames(outDF)[5] <- paste0(colnames(outDF)[5],"_", units(data$distance)$numerator)
-    write.csv(outDF, row.names=F, file = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"distanceBetweenLocations.csv"))
+    write.csv(outDF, row.names=F, file = appArtifactPath("distanceBetweenLocations.csv"))
   }
   return(data)
 }
