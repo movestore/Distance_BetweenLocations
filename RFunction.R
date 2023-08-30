f5f2c978700ae98e2de7e99b52662be536065631
annescharf <ascharf@ab.mpg.de>
2023-08-30 22:48:03 +0200

    fix allignmet

--- RFunction.R
+++ RFunction.R
@@ -4,18 +4,18 @@ library("ggplot2")
 library("sf")
 
 rFunction <-  function(data, units) { 
-      data$distance <- mt_distance(data, units=units)
-
-    if(mt_n_tracks(data)==1){
-      distanceHist <- ggplot(data[!is.na(data$distance),], aes(distance))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(data)]])+ xlab("Distance")+theme_bw()
-      pdf(appArtifactPath("distance_histogram.pdf"))
-      print(distanceHist)
-      dev.off()
+  data$distance <- mt_distance(data, units=units)
   
-      outDF <- data.frame(timestamps=mt_time(data),location.long=sf::st_coordinates(data)[,1],location.lat=sf::st_coordinates(data)[,2], track_id=mt_track_id(data), distance=data$distance)
-      colnames(outDF)[5] <- paste0(colnames(outDF)[5],"_", units(data$distance)$numerator)
-      write.csv(outDF, row.names=F, file = appArtifactPath("distanceBetweenLocations.csv"))
-    } else {
+  if(mt_n_tracks(data)==1){
+    distanceHist <- ggplot(data[!is.na(data$distance),], aes(distance))+geom_histogram(bins=100)+facet_grid(~.data[[mt_track_id_column(data)]])+ xlab("Distance")+theme_bw()
+    pdf(appArtifactPath("distance_histogram.pdf"))
+    print(distanceHist)
+    dev.off()
+    
+    outDF <- data.frame(timestamps=mt_time(data),location.long=sf::st_coordinates(data)[,1],location.lat=sf::st_coordinates(data)[,2], track_id=mt_track_id(data), distance=data$distance)
+    colnames(outDF)[5] <- paste0(colnames(outDF)[5],"_", units(data$distance)$numerator)
+    write.csv(outDF, row.names=F, file = appArtifactPath("distanceBetweenLocations.csv"))
+  } else {
     pdf(appArtifactPath("distance_histogram.pdf"))
     distanceHistAll <- ggplot(data[!is.na(data$distance),], aes(distance))+geom_histogram(bins=100)+ xlab("Distance")+ggtitle("All Individuals")+ theme_bw()
     print(distanceHistAll)
